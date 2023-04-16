ea77ddc29c1e8b1766307552fd5069ed9430ecdb
sanyuktaberad <sanyukta.berad@gmail.com>
2023-04-16 23:59:02 +0530

    Refactor: Code for creating a queue is added.

--- src/com/bridgelabz/StacksAndQueues.java
+++ src/com/bridgelabz/StacksAndQueues.java
@@ -1,42 +1,17 @@
 package com.bridgelabz;
 
-import java.util.Stack;
-import java.util.EmptyStackException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class StacksAndQueues {
-	static void pushData(Stack stack, int a) {
-		stack.push(new Integer(a));
-		System.out.println("Push(" + a + ")");
-		System.out.println("Stack :" + stack);
-	}
-
-	static void popData(Stack stack) {
-
-		System.out.println("pop ->");
-		Integer a = (Integer) stack.pop();
-		System.out.println(a);
-		System.out.println("Stack : " + stack);
-	}
-
 	public static void main(String[] args) {
-		Stack stack = new Stack();
-		System.out.println("Stack :" + stack);
+		Queue<Integer> queue = new LinkedList<>();
 
-		stack.push(56);
-		stack.push(30);
-		stack.push(70);
-		System.out.println("Element Of " + stack);
-		popData(stack);
-		popData(stack);
-		popData(stack);
+		queue.add(56);
+		queue.add(30);
+		queue.add(70);
 
-		System.out.println("Pop->" + stack.pop());
-		System.out.println("Peek ->" + stack.peek());
-		System.out.println("Check the stack is or not ?" + stack.empty());
-
-		
-		 
-
+		System.out.println("The Queue elements are : " + queue);
+		System.out.println("The Peek Of The Element :" + queue.peek());
 	}
-
 }
