ff74dac0cf5b370ae554d41d0a7c8f357ca595de
sanyuktaberad <sanyukta.berad@gmail.com>
2023-04-16 23:53:28 +0530

    Refactor: Code for adding and deleting the elements till it is empty is added.

--- src/com/bridgelabz/StacksAndQueues.java
+++ src/com/bridgelabz/StacksAndQueues.java
@@ -1,23 +1,42 @@
 package com.bridgelabz;
 
 import java.util.Stack;
+import java.util.EmptyStackException;
 
 public class StacksAndQueues {
 	static void pushData(Stack stack, int a) {
 		stack.push(new Integer(a));
 		System.out.println("Push(" + a + ")");
+		System.out.println("Stack :" + stack);
+	}
 
+	static void popData(Stack stack) {
+
+		System.out.println("pop ->");
+		Integer a = (Integer) stack.pop();
+		System.out.println(a);
+		System.out.println("Stack : " + stack);
 	}
 
 	public static void main(String[] args) {
 		Stack stack = new Stack();
 		System.out.println("Stack :" + stack);
 
-		stack.push(70);
-		stack.push(30);
 		stack.push(56);
+		stack.push(30);
+		stack.push(70);
+		System.out.println("Element Of " + stack);
+		popData(stack);
+		popData(stack);
+		popData(stack);
 
-		System.out.println("Element Is The : " + stack);
-		System.out.println("Top Element : " + stack.peek());
+		System.out.println("Pop->" + stack.pop());
+		System.out.println("Peek ->" + stack.peek());
+		System.out.println("Check the stack is or not ?" + stack.empty());
+
+		
+		 
+
 	}
+
 }
